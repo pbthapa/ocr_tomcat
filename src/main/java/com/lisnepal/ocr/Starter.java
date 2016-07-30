21096ae1cf2d65e122b5bac7cc74302763ba6d5c
pbthapa <ptpbthapa24@gmail.com>
2016-07-30 19:35:17 +0545

    added empty web.xml and modified application sub-class

--- src/main/java/com/lisnepal/ocr/Starter.java
+++ src/main/java/com/lisnepal/ocr/Starter.java
@@ -1,9 +1,18 @@
 package com.lisnepal.ocr;
 
+import java.util.Set;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("/")
+@ApplicationPath("/OCR")
 public class Starter extends Application {
+
+	@Override
+	public Set<Class<?>> getClasses() {
+		Set<Class<?>> resources = new java.util.HashSet<Class<?>>();
+		resources.add(com.lisnepal.rs.HelloWorld.class);
+		return resources;
+	}
 
 }
